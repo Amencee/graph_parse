329ff74f7e5c6a88d9beb8d896a44b24116b8afd
LiuJingQiang <129718321+LiuJingQiang@users.noreply.github.com>
2024-02-22 15:57:08 +0900

    init

---
+++ src/test/java/com/amence/graphparse/GraphParseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amence.graphparse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphParseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
